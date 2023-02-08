f580799a305503ea17780cc665d6524a6725d4fc
landpy <landpy@outlook.com>
2023-02-08 14:58:32 +0800

    Refine code.

--- _next/static/chunks/pages/home-7991af8b1fd18615.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[229],{7183:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home",function(){return u(7514)}])}},function(n){n.O(0,[34,675,346,514,774,888,179],(function(){return _=7183,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
